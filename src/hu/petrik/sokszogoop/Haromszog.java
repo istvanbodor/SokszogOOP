38b5dfa71e473916f1ed266a51f8886e0407c10e
Bodor István <istvanbodor19@gmail.com>
2022-10-04 14:37:58 +0200

    Haromszog osztaly - adattagok - getterek, setterek - kerulet, terulet - szerkeszthetőség vizsgáló füvvgény

---
+++ src/hu/petrik/sokszogoop/Haromszog.java
@@ -0,0 +1,57 @@
+package hu.petrik.sokszogoop;
+
+public class Haromszog extends Sokszog {
+    private double b;
+    private double c;
+
+    public Haromszog(double a, double b, double c) {
+        super(a);
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    private boolean isSzerkesztheto() {
+        boolean szerkeszthezo = true;
+        if ((this.getA() + this.b) <= this.getC() ||
+                (this.getA() + getC()) <= this.getB() ||
+                (this.getC() + this.c) <= this.getA()
+        ) {
+            szerkeszthezo = false;
+        }
+        return szerkeszthezo;
+    }
+
+
+    public double getKerulet() {
+        return this.getA() + this.getB() + this.getC();
+    }
+
+    private double getS() {
+        return getKerulet() / 2;
+    }
+
+    public double getTerulet() {
+        return Math.sqrt(this.getS() * (this.getS() - this.getA()) * (this.getS() - this.getB()) * (this.getS() - this.getC()));
+    }
+
+    @Override
+    public String toString() {
+        return "Haromszog{}";
+    }
+}
