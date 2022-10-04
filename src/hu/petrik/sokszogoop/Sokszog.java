c92681427d0b28cb1a6ec47a981d25ca9932a09e
Bodor István <istvanbodor19@gmail.com>
2022-10-04 14:21:08 +0200

    Sokszög osztály - a oldal getterek és setterek - egy paraméteres konstruktor
    Téglalap osztály
    - b oldal
    getterek és setterek
    két paraméteres konstruktor
    terület, kerület

---
+++ src/hu/petrik/sokszogoop/Sokszog.java
@@ -0,0 +1,21 @@
+package hu.petrik.sokszogoop;
+
+public class Sokszog {
+
+    private double a;
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public Sokszog(double a) {
+        this.a = a;
+    }
+
+
+
+}
---
+++ src/hu/petrik/sokszogoop/Teglalap.java
@@ -0,0 +1,32 @@
+package hu.petrik.sokszogoop;
+
+public class Teglalap extends Sokszog {
+    private double b;
+
+    public Teglalap(double a, double b) {
+        super(a);
+        this.b = b;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getKerulet() {
+        return 2 * (this.getA() + this.b);
+    }
+
+    public double getTerulet() {
+        return this.getA() * this.b;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Téglalap: a =%f - b = %f - K = %f - T =%f ",
+                this.getA(), this.getB(), this.getKerulet(), this.getTerulet());
+    }
+}
