5e715e36b12a3f4da3f2f89b90c8779d56026f86
Bodor István <istvanbodor19@gmail.com>
2022-10-05 20:00:21 +0200

    Negyzet osztaly

--- src/hu/petrik/sokszogoop/Negyzet.java
+++ src/hu/petrik/sokszogoop/Negyzet.java
@@ -1,4 +1,29 @@
 package hu.petrik.sokszogoop;
 
-public class Negyzet {
+public class Negyzet extends Sokszog {
+
+
+    public Negyzet (double a)
+    {
+        super(a);
+
+    }
+
+    @Override
+    public double getKerulet() {
+        return 4*getA();
+    }
+
+    @Override
+    public double getTerulet() {
+        return getA()*getA();
+    }
+
+    @Override
+    public String toString() {
+
+        return String.format("Négyzet: a =%-10.3f  %s ",
+                this.getA(),  super.toString());
+
+    }
 }
